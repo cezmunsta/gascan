ebe6fe59750f74a76c802d517354096f48cd3888
cezmunsta <github@incoming-email.co.uk>
2022-11-25 17:20:46 +0000

    Resolve basename of extras source files

--- scripts/ansible/init.sh
+++ scripts/ansible/init.sh
@@ -88,4 +88,4 @@ id -un "${USERUID}" || {
         "${USERNAME}"
 }
 
-setup "${PACKAGES}"
+setup "/opt/$(basename "${PACKAGES}")"
--- scripts/ansible/install.sh
+++ scripts/ansible/install.sh
@@ -5,8 +5,8 @@
 # PEX_UNZIP=1 PEX_ROOT=/tmp/foo ./ansible3.8 --help
 set -eu
 
-declare VERSION="${1:-3.9}"
-declare ANSIBLE="${2:-6.6.0}"
+declare -r VERSION="${1:-3.9}"
+declare -r ANSIBLE="${2:-6.6.0}"
 declare -r PACKAGES="${3:-undef}"
 declare -r REQUIREMENTS='/tmp/requirements.txt'
 
@@ -14,18 +14,20 @@ function build_pex {
     /app/venv/bin/pex -r "${REQUIREMENTS}" -c ansible -o "/app/ansible${VERSION}" -- --help
 }
 
-cat <<EOS > "${REQUIREMENTS}"
-ansible==${ANSIBLE}
-jmespath
-dnspython
-EOS
+function prep {
+    local -ar requirements=( "ansible==${ANSIBLE}" "jmespath" "dnspython" )
+    local -r extra_packages="${1}"
 
-if [ -f "/opt/${PACKAGES}" ]; then
-    cat "/opt/${PACKAGES}" >> "${REQUIREMENTS}"
-    rm -f "/opt/${PACKAGES}"
-fi
+    printf "%s\n%s\n%s\n" "${requirements[@]}" > "${REQUIREMENTS}"
 
-"/usr/bin/python${VERSION}" -m venv --clear /app/venv
-"/app/venv/bin/pip${VERSION}" install --quiet --upgrade pip pex wheel
+    if [ -f "/opt/${extra_packages}" ]; then
+        cat "/opt/${extra_packages}" >> "${REQUIREMENTS}"
+        rm -f "/opt/${extra_packages}"
+    fi
 
+    "/usr/bin/python${VERSION}" -m venv --clear /app/venv
+    "/app/venv/bin/pip${VERSION}" install --quiet --upgrade pip pex wheel
+}
+
+prep "$(basename "${PACKAGES}")"
 build_pex
