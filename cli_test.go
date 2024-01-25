20202fed2f8a378c856a3a336a74b1b1762238e0
cezmunsta <github@incoming-email.co.uk>
2024-01-25 14:55:10 +0000

    GAS-524 Issue extracting symlinks
    Files that are symlinks are not extracting correctly.
    
    * Fixed extraction of symlinks
    * Replaced use of io/ioutil with os
    * Updated minimum Go version

--- cli_test.go
+++ cli_test.go
@@ -33,27 +33,21 @@ func TestConfiguration(t *testing.T) {
 		case "EDITOR":
 			data["cfg"] = Config.Editor
 			data["exp"] = v
-			break
 		case "GASCAN_FLAG_LOG_LEVEL":
 			data["cfg"] = Config.LogLevel
 			data["exp"] = v
-			break
 		case "GASCAN_FLAG_PASSWORDLESS_SUDO":
 			data["cfg"] = Config.NoSudoPassword
 			data["exp"] = optInDefaultOff[v]
-			break
 		case "GASCAN_FLAG_PLAYBOOK":
 			data["cfg"] = Config.Playbook
 			data["exp"] = v
-			break
 		case "GASCAN_FLAG_SKIP_TAGS":
 			data["cfg"] = Config.SkipTags
 			data["exp"] = v
-			break
 		case "GASCAN_FLAG_TAGS":
 			data["cfg"] = Config.Tags
 			data["exp"] = v
-			break
 		}
 
 		if data["cfg"] != data["exp"] {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module gascan
 
-go 1.18
+go 1.21
