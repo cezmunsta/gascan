6ae02affba6dffaffb43db41901fa6c23f4516f5
Ceri Williams <github@incoming-email.co.uk>
2025-01-29 17:27:48 +0000

    GAS-760 Allow ad-hoc Ansible (#45)
    To help improve efficiency and reduce risks of leaving ad-hoc mode to
    the user.
    
    * Added --adhoc flag for direct Ansible usage and RunAnsible to execute
    the request at the end
    * Added makeAbsolutePaths to transform `~/` and `./` in the Ansible
    config so that it can be executed from anywhere
    * Updated flags to capture extra parameters to pass to Ansible
    * Updated extractBundle to avoid unnecessary files from being extracted
    to disk
    * Updated unit tests

--- os_test.go
+++ os_test.go
@@ -9,6 +9,23 @@ import (
 	"testing"
 )
 
+const ansibleDummyConfig = `
+[defaults]
+
+callback_plugins = ./plugins/callback
+callbacks_enabled = inspect
+
+inventory = ~/.config/gascan/inventory-config.json,~/.config/gascan/secrets.yaml
+inventory_plugins = ./plugins/inventory
+
+strategy_plugins = ./plugins/strategy
+strategy = locking
+
+[inventory]
+
+enable_plugins = dynamic_inventory_plugin,yaml
+`
+
 func generateDummyTarball() ([]byte, error) {
 	var buf bytes.Buffer
 	files := []struct {
@@ -82,25 +99,56 @@ func TestClearCache(t *testing.T) {
 	var err error
 
 	EnvCachePaths = "~,~/"
-	tmpDir := ""
+	cacheDir := ""
 
 	if err = clearInventoryCache(); err == nil {
 		t.Fatalf("expected an error for EnvCachePaths %q", EnvCachePaths)
 	}
 
-	if tmpDir, err = os.MkdirTemp(os.TempDir(), "cache1"); err != nil {
+	if cacheDir, err = os.MkdirTemp(os.TempDir(), "cache1"); err != nil {
 		t.Fatalf("unable to create directory: %v", err)
 	}
 
-	EnvCachePaths = tmpDir
+	defer os.RemoveAll(cacheDir)
 
-	if tmpDir, err = os.MkdirTemp(os.TempDir(), "cache2"); err != nil {
+	EnvCachePaths = cacheDir
+
+	if cacheDir, err = os.MkdirTemp(os.TempDir(), "cache2"); err != nil {
 		t.Fatalf("unable to create directory: %v", err)
 	}
 
-	EnvCachePaths += "," + filepath.Join(tmpDir, "foobar*") + "," + tmpDir
+	EnvCachePaths += "," + filepath.Join(cacheDir, "foobar*") + "," + cacheDir
 
 	if err = clearInventoryCache(); err != nil {
 		t.Fatalf("unexpected error for EnvCachePaths %q", EnvCachePaths)
+	}
+}
+
+func TestAnsibleConfig(t *testing.T) {
+	cfgDir := ""
+
+	if td, err := os.MkdirTemp(os.TempDir(), "cfg"); err == nil {
+		cfgDir = td
+	} else {
+		t.Fatalf("unable to create directory: %v", err)
+	}
+
+	defer os.RemoveAll(cfgDir)
+
+	dummyCfg := filepath.Join(cfgDir, "default.cfg")
+	newCfgName := filepath.Join(cfgDir, "new.cfg")
+
+	if err := os.WriteFile(dummyCfg, []byte(ansibleDummyConfig), 0o640); err != nil {
+		t.Fatalf("unable to write the dummy config: %v", err)
+	}
+
+	if newCfg := makeAbsolutePaths(dummyCfg, "new.cfg"); newCfg != newCfgName {
+		t.Fatalf("expected '%v', got '%v'", newCfgName, newCfg)
+	}
+
+	dummyCfg = newCfgName
+
+	if newContent, err := os.ReadFile(dummyCfg); err != nil || len(newContent) == 0 {
+		t.Fatalf("unable to read the new dummy config: %v", err)
 	}
 }
